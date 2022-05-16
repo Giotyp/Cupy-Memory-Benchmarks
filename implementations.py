f68733f9a4e76ca5fb842ccca9f94af5c0b529a2
Giotyp <typaldosgeorge@gmail.com>
2022-05-16 10:55:48 +0300

    Changed Implementations, fixed UM access

--- implementations.py
+++ implementations.py
@@ -1,51 +1,79 @@
 import numpy as np
 import cupy as cp
 from sys import getsizeof
+import ctypes
 
-def cpu_init(N):
-    a = np.random.rand(N,N).astype('float32')
-    b = np.random.rand(N,N).astype('float32')
-    c = np.random.rand(N,N).astype('float32')
-    a_gpu = cp.asarray(a)
-    b_gpu = cp.asarray(b)
+def gpu_copy(a_cpu, b_cpu):
+    '''
+    Take arrays a_cpu and b_cpu
+    Copy them to gpu and multiply them
+    Copy result back to cpu and increment its elements
+    '''
+
+    a_gpu = cp.asarray(a_cpu)
+    b_gpu = cp.asarray(b_cpu)
     res = cp.dot(a_gpu, b_gpu)
-    d = cp.asnumpy(res)
-    np.dot(c,d) 
     
-def gpu_init(N):
-    a_gpu = cp.random.randn(N,N,dtype='float32')
-    b_gpu = cp.random.randn(N,N,dtype='float32')
-    c = np.random.rand(N,N).astype('float32')
-    res = cp.dot(a_gpu, b_gpu)
-    d = cp.asnumpy(res)
-    np.dot(c,d) 
-    #print(res)
+    c = cp.asnumpy(res)
+    c[::] += 1
 
-def pool_stats(mempool):
-    print('used:',mempool.used_bytes(),'bytes')
-    print('total:',mempool.total_bytes(),'bytes\n')
-
-def unified_init(N):
-    dt = cp.dtype('float32').itemsize
+def unified_pointers(N, elmnts_type, num_pointers = 1):
+    '''
+    Return num_pointers  MemoryPointer instances to N bytes 
+    unified memory of elmnts_type 
+    '''
+    dt = elmnts_type.itemsize
     size = N*N*dt
     
-    ptr_a = cp.cuda.malloc_managed(size)
-    ptr_b = cp.cuda.malloc_managed(size)
-    return ptr_a, ptr_b
+    pointers = []
+    for i in range(num_pointers):
+        ptr = cp.cuda.malloc_managed(size)
+        pointers.append(ptr)
+    return pointers
 
-def unified(N,ptr_a, ptr_b):
+def unified_arrays(N,num_arrays):
+    '''
+    Return [[array_cpu, array_gpu],...] with arrays (N,N) 
+    from retrieved pointers to Unified Memory
+    '''
     shape = (N,N)
-    a = cp.ndarray(shape=shape, dtype=cp.float32, memptr=ptr_a)
-    for i in range(N):
-        a[i] = cp.random.randn(N,dtype='float32')
-    
-    
-    b = cp.ndarray(shape=shape, dtype=cp.float32, memptr=ptr_b)
-    for i in range(N):
-        b[i] = cp.random.randn(N,dtype='float32')
 
-    c = np.random.rand(N,N).astype('float32')
+    # Get array pointers
+    pointers = unified_pointers(N,np.dtype('float32'),num_arrays)
 
-    res = cp.dot(a, b)
-    d = cp.asnumpy(res)
-    cp.dot(c,d) 
+    arrays = []
+    for ptr in pointers:
+        # convert np.float32 to ctype_float
+        ctp = np.ctypeslib.as_ctypes_type(np.dtype('float32'))   
+        # cast pointers value to ctype POINTER of c_float  
+        pt = ctypes.cast(ptr.ptr,ctypes.POINTER(ctp))
+        # create numpy array from POINTER
+        x_cpu = np.ctypeslib.as_array(pt, shape)
+        # create cupy array from pointer ptr
+        x_gpu = cp.ndarray(shape=shape, dtype=np.dtype('float32'), memptr=ptr)
+
+        x_cpu[::] = np.random.rand(N,N).astype(np.dtype('float32')) 
+
+        arrays.append([x_cpu, x_gpu])
+    return arrays
+
+def unified(arrays):
+    '''
+    Take arrays arr_x = [x_cpu/x_gpu] in unified memory
+    where x = a, b, c from arrays=[arr_a, arr_b, arr_c]
+    Multiply them in gpu and get result also in UM
+    Increment elements of result in cpu
+    '''
+    dv = cp.cuda.Device(0)
+
+    arr_a, arr_b, arr_c = arrays
+
+    a_gpu = arr_a[1]
+    b_gpu = arr_b[1]
+    c_cpu, c_gpu = arr_c
+
+    c_gpu[::] = cp.dot(a_gpu, b_gpu) #result  in managed memory
+    dv.synchronize()
+
+    c_cpu[::] += 1 
+
--- test.py
+++ test.py
@@ -4,20 +4,19 @@ from cupyx.profiler import benchmark
 
 parser = argparse.ArgumentParser(description='Test different cupy memory tactics')
 parser.add_argument('-size', metavar='N', type=int, required=True, help='Array size used for testing')
-parser.add_argument('-cpu_init', action='store_true', help='Initialize to cpu and copy to gpu')
-parser.add_argument('-gpu_init', action='store_true', help='Initialize to gpu')
+parser.add_argument('-repeat', type=int, required=True, help='Benchmark repetition times')
+parser.add_argument('-gpu_copy', action='store_true', help='Initialize to cpu and copy to gpu')
 parser.add_argument('-unified', action='store_true', help='Use cupy unified memory')
 
 args = parser.parse_args()
 
 N = args.size
 
-if args.cpu_init:
-    print(benchmark(func=cpu_init,args=(N,),n_repeat=100))
-
-if args.gpu_init:
-    print(benchmark(func=gpu_init,args=(N,),n_repeat=100))
+if args.gpu_copy:
+    a_cpu = np.random.rand(N,N).astype(np.dtype('float32'))
+    b_cpu = np.random.rand(N,N).astype(np.dtype('float32'))
+    print(benchmark(func=gpu_copy,args=(a_cpu, b_cpu),n_repeat=args.repeat, n_warmup=2))
 
 if args.unified:
-    ptr_a, ptr_b = unified_init(N)
-    print(benchmark(func=unified,args=(N,ptr_a,ptr_b),n_repeat=100))
+    arrays = unified_arrays(N,3)
+    print(benchmark(func=unified,args=((arrays,)),n_repeat=args.repeat, n_warmup=2))
